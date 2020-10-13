8dbade50a27166d17441f8340b32121e1c4c3efc
ljonas <shamordinanton@gmail.com>
2020-10-13 16:32:05 +0300

    Registration window

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ let chat = document.querySelector('.chat');
 let oldMessageElement;
 let key = '0';
 
+function enterside()
+{
+    const registrationDiv = document.querySelector('.registrationdiv');
+    const enterDiv = document.querySelector('.enterdiv');
+    const registrationTab = document.querySelector('#regtab');
+    const enterTab = document.querySelector('#entertab');
+
+    registrationDiv.style.display = 'none';
+    enterDiv.style.display = 'inline';
+    registrationTab.style.zIndex = '0';
+    enterTab.style.zIndex = '2';
+}
+
+function registrationside()
+{
+    const registrationDiv = document.querySelector('.registrationdiv');
+    const enterDiv = document.querySelector('.enterdiv');
+    const registrationTab = document.querySelector('#regtab');
+    const enterTab = document.querySelector('#entertab');
+
+    registrationDiv.style.display = 'inline';
+    enterDiv.style.display = 'none';
+    registrationTab.style.zIndex = '2';
+    enterTab.style.zIndex = '0';
+}
+
 while (localStorage.getItem(key) != null)
 {
     oldMessageElement = document.createElement('p');
